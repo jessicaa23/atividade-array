8d0f975ccd0c7073a6860a97401293d52e5a1db3
jessicasilva <109664683+jessicaa23@users.noreply.github.com>
2023-04-10 12:21:04 -0300

    Add files via upload

---
+++ teste2/script.js
@@ -0,0 +1,93 @@
+
+//const person = ["felipe", 30, "são paulo", "masculino", "123456787"];
+
+//person.pop();
+
+//console.log(person);
+
+
+
+//const fruits = ["apple", "orange", "banana", "pear"];
+
+//fruits.shift();
+
+//console.log(fruits);
+
+
+ 
+//const jobs = ["frontend", "backend", "devOps", "FullStack"];
+
+//jobs.unshift("design");
+//console.log(jobs);
+
+
+
+//const jobs = ["Frontend", "Backend", "DevOps", "FullStack"];
+
+//let indexOf = jobs.indexOf("Frontend");
+
+//console.log(indexOf);
+
+
+//const fruits = ["apple", "orange", "banana", "pear"];
+//let indexOfSearch = fruits.indexOf("orange", 3);
+
+//console.log(indexOfSearch);
+
+
+//const jobs = ["Frontend", "Backend", "DevOps", "FulltStack"];
+
+//jobs.sort(); 
+
+//console.log(jobs);
+
+
+
+//const colors = ["Red", "Green", "White", "Black"];
+//let join = colors.join("+");
+
+//console.log(join);
+
+
+
+
+//ATIVIDADE!!!!!!!!!!
+
+//const fruits =["apple", "orange", "banana", "pear"];
+//let join = fruits.join("/")
+//console.log(join);
+
+
+//const names = ["cecilia", "melanie", "jayne", "leticia", "ana"];
+
+//names.sort(); 
+
+//console.log(names);
+
+
+
+//escreva uma função que recebe duas String, digamos a e b, como um argumentos
+
+//se a contém b, anexe b ao inicio de a, se não, anexe b ao final e retorne a concentraçãorder
+
+
+//function myFunction(a, b){
+    //let array = [];
+
+   // if(a.indexOf(b) !== -1){
+       // array.push(b + a);
+        //return array;
+    //} else{
+        //array.push(a + b);
+       // return array;
+    //}
+//}
+//console.log(myFunction("types", "s"))
+
+
+
+const animal = ["gato", "cachorro", "papagaio", "tigre"];
+
+animal.shift();
+
+console.log(animal);
